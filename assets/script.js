40c3351694afdbff0e944c41f4dd5f92b63630ca
Jaimie <jaimie.j.hemmings@gmail.com>
2024-09-19 11:23:08 +0100

    add: initial Three.js Set up

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,29 @@
-import * as THREE from 'three';
+import * as THREE from "three";
+import { OrbitControls } from 'jsm/controls/OrbitControls.js';
+import { MeshLine, MeshLineGeometry, MeshLineMaterial } from "./MeshLine/index.js";
+import { UnrealBloomPass } from 'jsm/postprocessing/UnrealBloomPass.js';
+import { EffectComposer } from 'jsm/postprocessing/EffectComposer.js';
+import { RenderPass } from 'jsm/postprocessing/RenderPass.js';
 
+const w = window.innerWidth;
+const h = window.innerHeight;
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
-
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-
+const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
 camera.position.z = 5;
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(w, h);
+document.body.appendChild(renderer.domElement);
 
-function animate() {
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.03;
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-	renderer.render( scene, camera );
+const textLoader = new THREE.FontLoader();
+const points = [];
+const numPoints = 300;
+for (let j =0; j < numPoints; j++) {
+  let x = -7.5 + j * 0.05;
+  points.push(x, y, 0);
 }
-renderer.setAnimationLoop( animate );
+const geometry = new MeshLineGeometry();
+geometry.setPoints(points);
